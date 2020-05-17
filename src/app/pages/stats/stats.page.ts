7e71ce492bfdbd286a00b7f772adc107c7fcf18c
Diego <gomezefdiego@gmail.com>
2020-05-17 10:28:34 -0600

    GameStats finished

--- src/app/pages/stats/stats.page.ts
+++ src/app/pages/stats/stats.page.ts
@@ -18,11 +18,15 @@ export class StatsPage implements OnInit {
   games: any = [
 
   ];
+  gamesselect: any = [
 
-  constructor(private calendar: NgbCalendar, private http: HttpClient, private modalCtrl:ModalController) { }
+  ];
+
+  constructor(private calendar: NgbCalendar, private http: HttpClient, private modalCtrl: ModalController) { }
 
   ngOnInit() {
     this.getData();
+    this.date = null;
   }
   sortBy(key) {
     console.log(key);
@@ -37,36 +41,45 @@ export class StatsPage implements OnInit {
     this.filterData();
   }
 
-  async graph(data){
+  async graph(data) {
     const modal = await this.modalCtrl.create({
-      component:ChartPage,
-      componentProps:{
-        movs:data
+      component: ChartPage,
+      componentProps: {
+        movs: data
       }
     });
     modal.present();
   }
 
-  async graphgame(data,i){
+  async graphgame(data, i) {
     const modal = await this.modalCtrl.create({
-      component:ChartPage,
-      componentProps:{
-        movs:data,
-        index:i
+      component: ChartPage,
+      componentProps: {
+        movs: data,
+        index: i
       }
     });
     modal.present();
   }
 
   filterData() {
-    console.log("Filtering: " + this.date + ", " + this.juego);
-    //this.getData(this.date,this.juego);
-  }
+    let filter;
+    if (this.date != null) {
+      let day;
+      let month;
+      if (this.date.month < 10) month = "0" + this.date.month;
+      else month = this.date.month + "";
+      if (this.date['day'] < 10) day = "0" + this.date['day'];
+      else day = this.date['day'] + "";
+      filter = {date:this.date.year+"-"+month+"-"+day, game:this.juego};
+    }else{
+      filter = {date: this.date, game:this.juego};
+    }
 
-  getData() {
-    this.http.get('http://68.183.30.44:4000/api/matches').subscribe(
+    this.http.post('http://68.183.30.44:4000/api/matchF',filter).subscribe(
       res => {
-        this.games = res;
+        if(this.isIterable(res))this.games = res;
+        else this.games = [res];
         this.games.forEach(element => {
           element['isCollapsed'] = true;
           element.movs.forEach(mov => {
@@ -82,6 +95,44 @@ export class StatsPage implements OnInit {
                 break;
               case 4:
                 mov.dscrp="Izquierda";
+                break;
+            }
+          });
+        });
+      },
+      err => console.log(err)
+    )
+
+  }
+
+  isIterable(obj) {
+    // checks for null and undefined
+    if (obj == null) {
+      return false;
+    }
+    return typeof obj[Symbol.iterator] === 'function';
+  }
+
+  getData() {
+    this.http.get('http://68.183.30.44:4000/api/matches').subscribe(
+      res => {
+        this.games = res;
+        this.gamesselect = this.games;
+        this.games.forEach(element => {
+          element['isCollapsed'] = true;
+          element.movs.forEach(mov => {
+            switch (mov.mov) {
+              case 1:
+                mov.dscrp = "Adelante";
+                break;
+              case 2:
+                mov.dscrp = "Derecha";
+                break;
+              case 3:
+                mov.dscrp = "Atras";
+                break;
+              case 4:
+                mov.dscrp = "Izquierda";
                 break;
             }
           });
